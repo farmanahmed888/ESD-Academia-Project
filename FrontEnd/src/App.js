2808b1065046f9ced57f1a586730788e6918b095
Farman Ahmed <farmanahmed888@gmail.com>
2024-08-17 05:54:34 +0530

    chore: Update dependencies and fix formatting in App.js

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,13 +1,18 @@
-import './App.css';
+import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Navbar from './layout/Navbar';
-import Home from './pages/Home';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from "./layout/Navbar";
+import Home from "./pages/Home";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import { useSelector } from "react-redux";
-import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import Login from './pages/Login';
-import Student from './pages/Student';
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import Login from "./pages/Login";
+import Student from "./pages/Student";
 
 // ProtectedRoute component
 const ProtectedRoute = ({ children }) => {
@@ -16,23 +21,55 @@ const ProtectedRoute = ({ children }) => {
 
   useEffect(() => {
     if (!token) {
-      navigate('/');
+      navigate("/");
     }
   }, [token, navigate]);
 
   return token ? children : null;
-}
+};
+const RedirectIfLoggedIn = ({ children }) => {
+  const token = useSelector((state) => state.auth.jwtToken);
+  const navigate = useNavigate();
 
+  useEffect(() => {
+    if (token) {
+      navigate("/Home");
+    }
+  }, [token, navigate]);
+
+  return token ? null : children;
+};
 function App() {
   return (
     <div className="App">
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Login />} />
+          <Route
+            path="/"
+            element={
+              <RedirectIfLoggedIn>
+                <Login />
+              </RedirectIfLoggedIn>
+            }
+          />
           {/* Protected Routes */}
-          <Route path="/Home" element={<ProtectedRoute><Home /></ProtectedRoute>} />
-          <Route path="/Student" element={<ProtectedRoute><Student /></ProtectedRoute>} />
+          <Route
+            path="/Home"
+            element={
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/Student"
+            element={
+              <ProtectedRoute>
+                <Student />
+              </ProtectedRoute>
+            }
+          />
         </Routes>
       </Router>
     </div>
